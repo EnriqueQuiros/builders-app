d4fa6ca47685846c4a8764e1cf6aef5f7b20d5ed
Ekri Fos <enriquequirosfernandez@gmail.com>
2021-08-14 19:11:15 +0200

    Added Search bar component

--- src/components/Companies.tsx
+++ src/components/Companies.tsx
@@ -1,14 +1,24 @@
+import React from "react";
 import ICompany from "../interfaces";
 import Company from "./Company";
 import { useGetCompanies } from "../hooks/useGetCompanies";
+import SearchBar from "./SearchBar";
 
 const Companies = () => {
   const { data, status } = useGetCompanies();
+  const [searchText, setSearchText] = React.useState("");
+
+  const search = (newText: string) => setSearchText(newText);
+
+  React.useEffect(() => {
+    console.log("SEARCH ", searchText);
+  }, [searchText]);
 
   return (
     <>
-      <h2>Companies</h2>
-      <p>Status: {status}</p>
+      <h1 className="text-2xl font-bold text-gray-700 mt-6">Companies</h1>
+
+      <SearchBar onSearch={search} />
 
       {status === "error" && <div>Error fetching data</div>}
 
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,36 @@
+export interface SearchBarProps {
+  onSearch(searchText: string): void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = (props) => {
+  return (
+    <>
+      <div className="flex max-w-md mx-auto  bg-white rounded-lg shadow-lg mt-10">
+        <span className="  px-2 flex items-center pl-3">
+          <svg
+            className="w-5 h-5 text-gray-400"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+          </svg>
+        </span>
+
+        <input
+          type="text"
+          className="w-full py-2 px-8  text-gray-700 bg-white rounded-md focus:outline-none focus:ring ring-gray-300 "
+          placeholder="Search"
+          onChange={(e) => props.onSearch(e.target.value)}
+        />
+      </div>
+    </>
+  );
+};
+
+export default SearchBar;
